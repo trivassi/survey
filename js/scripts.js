37d6bd32b7329a7a126b24f59860b072163e8386
Addison Nishijima and Thomas Rivas <addison@>
2016-08-10 16:48:35 -0700

    fix css styling for survey project

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 //Everything below this line is back-end logic:
+$("form").submit(function(event) {
+  event.preventDefault();
 
+  var pizza = $("#pizza").val();
+  //radiobuttons
+  var flavor = $("input:radio[name=flavor]:checked").val();
+  //calendar
+  var dob = $("#born").val();
+  //choosecolor
+  var favoriteColor = $("#color").val();
 
 //Everything below this line is front-end logic:
-
-  $("form").submit(function(event) {
-    event.preventDefault();
-
-    var pizza = $("#pizza").val();
-    //radiobuttons
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    //calendar
-    var dob = $("#born").val();
-    //choosecolor
-    var favoriteColor = $("#color").val();
 
     // console.log(pizza);
     $("#outputpizza").text(pizza);
